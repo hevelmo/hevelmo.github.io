ac601d6c1909d388617474f5f5790b099d95e310
Heriberto <hevelmo060683@gmail.com>
2022-10-24 12:28:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-<?php
-print_r('Entra');
-?>
 <!DOCTYPE html>
 <html lang="es" dir="ltr">
     <head>
         <meta charset="utf-8">
         <title>hevelmo</title>
-        <link rel="stylesheet" href="css/core.css">
+        <link rel="stylesheet" href="assets/css/core.css">
         <link rel="shortcut icon" type="image/png" href="img/favicon/favicon.ico"/>
     </head>
     <body>
