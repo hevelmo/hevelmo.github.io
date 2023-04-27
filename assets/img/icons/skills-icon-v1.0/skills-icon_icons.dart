c87e10399a7c5d749a13882594d0b1fa4c9e62eb
hevelmo <hevelmo060683@gmail.com>
2023-04-27 09:13:32 -0600

    c

---
+++ assets/img/icons/skills-icon-v1.0/skills-icon_icons.dart
@@ -0,0 +1,28 @@
+// Place fonts/skills-icon.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: skills-icon
+//      fonts:
+//       - asset: fonts/skills-icon.ttf
+import 'package:flutter/widgets.dart';
+
+class Skills_icon {
+  Skills_icon._();
+
+  static const String _fontFamily = 'skills-icon';
+
+  static const IconData adobe_dreamweaver_cc = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData adobe_photoshop_cc = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData adobe_illustrator_cc = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData adobe_xd_cc = IconData(0xe908, fontFamily: _fontFamily);
+  static const IconData figma = IconData(0xe90b, fontFamily: _fontFamily);
+  static const IconData lunacy = IconData(0xe911, fontFamily: _fontFamily);
+  static const IconData bootstrap = IconData(0xe913, fontFamily: _fontFamily);
+  static const IconData ajax = IconData(0xe914, fontFamily: _fontFamily);
+  static const IconData son = IconData(0xe91a, fontFamily: _fontFamily);
+  static const IconData jquery = IconData(0xe91b, fontFamily: _fontFamily);
+  static const IconData handlebars = IconData(0xe91c, fontFamily: _fontFamily);
+  static const IconData slim = IconData(0xe91d, fontFamily: _fontFamily);
+  static const IconData twig = IconData(0xe91f, fontFamily: _fontFamily);
+}
